7664f6c2f506c263aa157669e61e91af9da57a67
CallumCheng <callumcheng@outlook.com>
2022-08-31 12:59:36 +0100

    create home page + github users page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+import * as Pages from './pages';
 
 function App() {
   
   return (
     //Route paths go here
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Pages.HomePage/>} />
+        <Route path='/user'>
+          <Route path=':username' element={<Pages.UserPage/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
